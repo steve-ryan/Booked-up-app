6dbaec710e012c9c9aa14fab1f3039855b578543
stephen wachira <stevohyoung@gmail.com>
2019-09-16 03:29:42 +0300

    assignment

---
+++ Book.js
@@ -0,0 +1,12 @@
+class Book {
+    constructor(title, author, pages, description, currentPage, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.description = description;
+        this.currentPage = currentPage;
+        this.read = read;
+    }
+}
+
+export { Book };
---
+++ database.js
@@ -0,0 +1,5 @@
+import { Book } from './Book.js';
+
+
+
+export { bookList };
